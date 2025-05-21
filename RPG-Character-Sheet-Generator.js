a74b295ccbc79d8e4551e4ebacfc60109a36fffe
Simi0M0 <somotoso@Outlook.com>
2025-05-21 18:12:57 -0400

    RPG Character Sheet Generator
    Practiced with variables & logging

---
+++ RPG-Character-Sheet-Generator.js
@@ -0,0 +1,17 @@
+const characterName = "Simi";
+const characterClass = "Mage";
+let characterLevel = 1;
+let healthPoints = 45;
+let isCharacterAlive = true;
+let manaPoints = 100;
+
+characterLevel += 1;
+healthPoints += 5;
+manaPoints += 5;
+
+console.log(characterName)
+console.log(characterClass)
+console.log(characterLevel)
+console.log(healthPoints)
+console.log(isCharacterAlive)
+console.log(manaPoints)
