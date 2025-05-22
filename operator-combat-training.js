a35f2eeb3e2a8f0543fed66fb5d0b37143ce9908
Simi0M0 <somotoso@Outlook.com>
2025-05-21 21:00:52 -0400

    Operator Combat Training
    practiced with operators

---
+++ operator-combat-training.js
@@ -0,0 +1,23 @@
+let playerHealth = 50;
+const damageTaken = 5;
+let hasShield = false;
+let isEnemyNearby = true;
+
+console.log("Add:", playerHealth + damageTaken);
+console.log("Subtract: ", playerHealth -= damageTaken);
+console.log("Multiply: ", playerHealth * damageTaken);
+console.log("Divide: ", playerHealth / damageTaken);
+console.log("Remainder: ", playerHealth % damageTaken);
+console.log("Exponent: ", playerHealth ** damageTaken);
+
+console.log("Loose:", playerHealth == String(playerHealth));
+console.log("Strict: ", playerHealth === String(playerHealth));
+console.log("Not Equal: ", playerHealth != damageTaken);
+console.log("Greater Than or Equal To: ", playerHealth >= damageTaken);
+
+let score = 30;
+
+console.log("Increase By Fixed Number: ", );
+console.log();
+console.log();
+console.log();
